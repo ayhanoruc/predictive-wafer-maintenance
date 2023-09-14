a3a8a520eb841af3a81a9ce988fba7ceb734ccc4
ayhanoruc <ayhan.orc.2554@gmail.com>
2023-09-15 00:44:10 +0300

    error & log handling for data ingestion & data validation done!

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,42 +1,43 @@
 import pandas as pd 
-import numpy as np 
-from sklearn.model_selection import train_test_split
 
-import os, sys 
-import logging
-
-
-
-from src.exception_handler import CustomException, handle_exceptions
+from src.exception_handler import handle_exceptions
 from src.log_handler import AppLogger 
 from src.entity.config_entity import DataIngestionConfig
 from src.entity.artifact_entity import DataIngestionArtifact 
-from src.constants.training_pipeline import TRAINING_SCHEMA_FILE_PATH
 from src.data_access.mongo_db import MongoConnect
-from src.constants.mongo import MONGO_URL,DB_NAME,TRAINING_COLLECTION_NAME
-
-from src.utility.generic import write_to_csv, export_csv_to_df
-
+from src.constants.mongo import MONGO_URL,DB_NAME
 
 
 
 class DataIngestionComponent:
-    
-    
+        """
+    A class for ingesting raw data into the feature store.
+
+    Args:
+        data_ingestion_config (DataIngestionConfig): Configuration for data ingestion.
+
+    Attributes:
+        log_writer (AppLogger): Logger object for logging data ingestion progress.
+        data_ingestion_config (DataIngestionConfig): Data ingestion configuration.
+
+    Methods:
+        export_raw_data_into_feature_store: Ingests raw data into the feature store.
+        run_data_ingestion: Runs the data ingestion process and returns the DataIngestionArtifact.
+    """
+
         def __init__(self,data_ingestion_config:DataIngestionConfig):
             self.log_writer = AppLogger(phase_name="DataIngestion")
 
             self.data_ingestion_config = data_ingestion_config
        
 
-
-
-
-
-
-
+       
         @handle_exceptions
         def export_raw_data_into_feature_store(self):
+                
+            """
+            Ingests raw data into the feature store.
+            """
             connection = MongoConnect() 
             connection.mongo_connection(MONGO_URL,DB_NAME) 
             #artifact > timestamp > feature_store | ingested_data 
@@ -44,40 +45,38 @@ class DataIngestionComponent:
             connection.ingest_with_fs(target_dir=self.data_ingestion_config.feature_store_dir,collection_name=self.data_ingestion_config.training_collection_name)
             self.log_writer.handle_logging("RAW DATA INGESTION ENDED")
             
-
-
         
+        """
+        ---NOT USED, LEAVED HERE FOR REFERENCE FOR LATER PROJECT ---
 
         @handle_exceptions
         def split_train_test(self,df:pd.DataFrame)->None:
-            """
+                
+            
                 This method takes a DataFrame, splits it into train and test sets using the specified split ratio,
                 and then saves the resulting train and test sets to the corresponding file paths provided in
                 the data_ingestion_config. If an error occurs during the process, a CustomException is raised,
                 and the error is logged.
-            """
+            
             #takes dataframe , splits it into train and test files then sends them to ingested folder
 
+
             train_set, test_set = train_test_split(df,test_size=self.data_ingestion_config.split_ratio,random_state=19)
-            
+            self.log_writer.handle_logging("dataset split into train & test")
             write_to_csv(train_set,file_path=self.data_ingestion_config.training_file_path)
-            write_to_csv(test_set,file_path=self.data_ingestion_config.testing_file_path)
-
-
-        
-
-
-
+            write_to_csv(test_set,file_path=self.data_ingestion_config.testing_file_path)"""
 
 
         @handle_exceptions
         def run_data_ingestion(self)->DataIngestionArtifact:
+            """
+            Runs the data ingestion process.
+
+            Returns:
+                DataIngestionArtifact: The data ingestion artifact containing feature store and file paths.
+            """
             self.log_writer.handle_logging("-------------ENTERED RAW DATA INGESTION STAGE------------")
             self.export_raw_data_into_feature_store() 
-            #here merge operation should come into play: merge multiple dataframes into single dataframe
-            #csv_path_list=[] # this should come from validated data artifact
-            #merge_csv_files(csv_path_list)
-            #self.split_train_test(df)
 
             data_ingestion_artifact= DataIngestionArtifact(
                 feature_store= self.data_ingestion_config.feature_store_dir,
@@ -85,6 +84,8 @@ class DataIngestionComponent:
                 test_file_path = self.data_ingestion_config.testing_file_path,
 
             )
+            self.log_writer.handle_logging("CREATED data ingestion artifact successully")
+
             return data_ingestion_artifact 
 
 
--- src/components/data_validation.py
+++ src/components/data_validation.py
@@ -1,51 +1,76 @@
 import pandas as pd 
 
-import os 
-import shutil
-
 from src.exception_handler import CustomException, handle_exceptions
 from src.log_handler import AppLogger
 from src.entity.config_entity import DataValidationConfig
 from src.entity.artifact_entity import DataIngestionArtifact, DataValidationArtifact
-from src.utility.generic import read_json_file
-from src.constants.training_pipeline import TRAINING_SCHEMA_FILE_PATH, PREDICTION_SCHEMA_FILE_PATH
-from src.utility.generic import create_regex, check_regex_match
+from src.utility.generic import create_regex, check_regex_match , read_json_file
 from src.data_access.mongo_db import MongoConnect
-from src.constants.training_pipeline import MONGO_VALID_COLLECTION_NAME, MONGO_INVALID_COLLECTION_NAME
+from src.constants.training_pipeline import (MONGO_VALID_COLLECTION_NAME, MONGO_INVALID_COLLECTION_NAME,
+                                              TRAINING_SCHEMA_FILE_PATH, PREDICTION_SCHEMA_FILE_PATH)
 from src.constants.mongo import MONGO_URL,DB_NAME
+
+import os 
+import shutil
 
 
 class DataValidationComponent:
 
     def __init__(self,data_validation_config:DataValidationConfig, data_ingestion_artifact:DataIngestionArtifact):
+
+        # Initialize the logger, configuration, and other attributes
+        self.log_writer = AppLogger("DataValidation")
+
         self.data_validation_config = data_validation_config
         self.data_ingestion_artifact = data_ingestion_artifact
-        self.training_schema = read_json_file(TRAINING_SCHEMA_FILE_PATH) # data sharring agreement schema
+
+        # Load schemas and other parameters from JSON files
+        self.training_schema = read_json_file(TRAINING_SCHEMA_FILE_PATH) # data sharing agreement schema for training
+        self.log_writer.handle_logging("read TRAINING SCHEMA succesfully")
         self.sample_file_name = self.training_schema["SampleFileName"]
-        self.date_length = self.training_schema["LengthOfDateStampInFile"]
+        #self.date_length = self.training_schema["LengthOfDateStampInFile"]
         self.timestamp_length= self.training_schema["LengthOfTimeStampInFile"]
         self.training_cols= self.training_schema["col_name"]
         self.missing_threshold= self.training_schema["MissingThreshold"]
-        self.prediction_schema = read_json_file(PREDICTION_SCHEMA_FILE_PATH)
+
+        self.prediction_schema = read_json_file(PREDICTION_SCHEMA_FILE_PATH) # data sharing agreement schema for prediction
+        self.log_writer.handle_logging("read PREDICTION SCHEMA succesfully")
         self.prediction_cols = self.prediction_schema["col_name"]
 
+        self.re_obj = None
 
+        self.df = None
 
-        self.log_writer = AppLogger("DataValidation")
+        #connect to mongodb
+        try:
+            self.mongo_connect = MongoConnect()
+            self.mongo_connect.mongo_connection(MONGO_URL,DB_NAME)
 
-        self.mongo_connect = MongoConnect()
-        self.mongo_connect.mongo_connection(MONGO_URL,DB_NAME)
+        except Exception as e:
+            CustomException("Couldnt succesfully connected to mongodb",e) 
+
 
         
 
     
     @handle_exceptions
-    def validate_file_name(self,file_path,is_prediction=False)->bool:
-        
+    def validate_file_name(self,file_path)->bool: 
+        """
+        Validates the filename using a regular expression.
+
+        Args:
+            file_path (str): The path of the file to validate.
+
+        Returns:
+            bool: True if the filename matches the expected pattern, False otherwise.
+        """
         file_name= os.path.basename(file_path)
         self.log_writer.handle_logging(f"Filename validation stage for {file_name}")
-        re_obj= create_regex(is_prediction)
-        return check_regex_match(re_obj,file_name) # returns boolean
+
+        #re_obj= create_regex(is_prediction) # this can be turned into instance object for the datavalidation class
+        #self.log_writer.handle_logging("Related regex pattern compiled succesfully .")
+
+        return check_regex_match(self.re_obj,file_name) # returns boolean, [re_obj -> self.re_obj]
     
     
 
@@ -68,9 +93,19 @@ class DataValidationComponent:
 
     @handle_exceptions
     def validate_columns_2(self,file_path, is_prediction=False):
-        df = pd.read_csv(file_path)
+        """
+        Validates the columns and data types of a DataFrame loaded from a file.
+
+        Args:
+            file_path (str): The path of the CSV file to validate.
+            is_prediction (bool, optional): True if validating prediction data, False for training data. Defaults to False.
+
+        Returns:
+            bool: True if the columns and data types match the expected schema, False otherwise.
+        """
+        #df = pd.read_csv(file_path) # --> should be instance attribute since shared among instance methods.
         self.log_writer.handle_logging(f"Column number and dtype validation stage for {os.path.basename(file_path)}")
-        df_columns= set(df.columns)
+        df_columns= set(self.df.columns)
         if is_prediction:
             expected = self.prediction_cols
         else:
@@ -79,12 +114,13 @@ class DataValidationComponent:
         missing_cols = set(expected.keys()) - df_columns
 
         if missing_cols:
-            print("missing cols: ",missing_cols)
+            #print("missing cols: ",missing_cols)
+            self.log_writer.handle_logging(f"missing cols: {missing_cols}")
             return False 
         
         for col_name, expected_dtype in expected.items():
-            if str(df[col_name].dtype) not in expected_dtype:
-                print(col_name,df[col_name].dtype, expected_dtype,": are missing")
+            if str(self.df[col_name].dtype) not in expected_dtype:
+                self.log_writer.handle_logging(f"Dtype un-match detected {col_name}, {self.df[col_name].dtype} != {expected_dtype}")
                 return False
         
         return True
@@ -92,39 +128,79 @@ class DataValidationComponent:
 
     @handle_exceptions
     def check_missing_value_percentage(self,file_path):
-        df = pd.read_csv(file_path)
-        self.log_writer.handle_logging(f"Missing Value Check stage for {os.path.basename(file_path)}")
-        missing_percentage = (df.isna().sum().sum())/(df.shape[0]*df.shape[1]) # for each column
-        self.log_writer.handle_logging(f"Missing value % {missing_percentage}")
+        """
+        Checks the percentage of missing values in a DataFrame loaded from a file.
+
+        Args:
+            file_path (str): The path of the CSV file to check.
+
+        Returns:
+            bool: True if the missing value percentage is below the threshold, False otherwise.
+        """
         
+        #df = pd.read_csv(file_path)
+        self.log_writer.handle_logging(f"Missing Value Check stage for {os.path.basename(file_path)}")
+
+        missing_percentage = (self.df.isna().sum().sum())/(self.df.shape[0]*self.df.shape[1]) # for the entire dataset
+        self.log_writer.handle_logging(f"Missing value % {missing_percentage}")
         
         #print(any(above_threshold))
         """if any(above_threshold):
             result = {col: above_thresh for col, above_thresh in above_threshold.items()}
-            #then log the results here """
+            #then log the results here """ # -> this logic can be adjusted for individual column check
             
         return missing_percentage > self.missing_threshold
 
     @handle_exceptions
     def check_zero_std(self,file_path):
-        df= pd.read_csv(file_path)
+        """
+        Checks if any column in the DataFrame has zero standard deviation.
+
+        Args:
+            file_path (str): The path of the CSV file to check.
+
+        Returns:
+            bool: Always returns False, as this check is for preprocessing, not validation.
+        """
+        """df= pd.read_csv(file_path)
         df_std = df.iloc[:,1:-1].std()
         zero_std_cols = df_std[df_std==0].index 
-        """if any(zero_std_cols):
+        if any(zero_std_cols):
             pass 
             #log here
-        #return any(zero_std_cols)"""
+        #return any(zero_std_cols)""" # i commented out this part, since thats a preprocessing job, not a validation.
+        # therefore, return False always, or just remove this part.
         return False
     
 
 
     @handle_exceptions
     def data_drift_check(self):
+        """
+        Performs data drift checks.
+
+        Returns:
+            bool: Always returns True for now.
+        """
         return True 
 
     
     @handle_exceptions
     def check_all_conditions(self, file_path,is_prediction=False):
+        """
+        Checks all validation conditions for a given file.
+
+        Args:
+            file_path (str): The path of the file to validate.
+            is_prediction (bool, optional): True if validating prediction data, False for training data. Defaults to False.
+
+        Description:
+            This method performs a series of data validation checks for the specified file, including filename validation,
+            column validation, missing value percentage, and zero standard deviation checks. Depending on the validation results,
+            the file may be moved to either the valid or invalid data directory.
+        """
+
+        #!!! the following if-else conditionals can be move outside this loop element since we choose prediction or training only once for a pipeline.
         if is_prediction:
             valid_data_path = self.data_validation_config.valid_unseen_data_dir
             invalid_data_path = self.data_validation_config.invalid_unseen_data_dir
@@ -133,12 +209,19 @@ class DataValidationComponent:
             valid_data_path = self.data_validation_config.valid_training_data_dir
             invalid_data_path = self.data_validation_config.invalid_training_data_dir
 
+        self.re_obj = create_regex(is_prediction=is_prediction)
+        self.log_writer.handle_logging("Related regex pattern compiled succesfully .")
+
+        self.df = pd.read_csv(file_path)
+        self.log_writer.handle_logging("dataframe has been read succesfully!")
+
         os.makedirs(valid_data_path,exist_ok=True)
         os.makedirs(invalid_data_path,exist_ok=True)
+        self.log_writer.handle_logging(f"directories succesfully created 1-{valid_data_path} 2-{invalid_data_path}")
 
+        self.log_writer.handle_logging("Validation Check Stages now starting...")
 
-        self.log_writer.handle_logging("Validation Check Stages is started...")
-        if self.validate_file_name(file_path,is_prediction):
+        if self.validate_file_name(file_path):
             self.log_writer.handle_logging(f"Filename Validated!")
             if self.validate_columns_2(file_path,is_prediction):
                 self.log_writer.handle_logging(f"Columns Validated!")
@@ -146,11 +229,13 @@ class DataValidationComponent:
                     self.log_writer.handle_logging(f"Missing Value Validation Passed!")
 
                     if not self.check_zero_std(file_path):
-                        print("check_zero")                        
+                        #print("check_zero")                        
                         shutil.move(file_path,os.path.join(valid_data_path,os.path.basename(file_path))) # öncesinde copy yapıp sonra move da edilebilir.
+                        self.log_writer.handle_logging(f"{os.path.basename(file_path)} has been validated and moved to related valid dataset directory")
                     
                     else:
                         shutil.move(file_path,os.path.join(invalid_data_path,os.path.basename(file_path)))
+                        self.log_writer.handle_logging(f"File {os.path.basename(file_path)} is moved to invalid data directory")
 
                 else:
 
@@ -173,6 +258,13 @@ class DataValidationComponent:
 
     @handle_exceptions
     def post_validation_insertion(self):
+        """
+        Inserts valid and invalid files into MongoDB.
+
+        Description:
+            This method inserts the files located in the valid and invalid data directories
+            into MongoDB collections designated for valid and invalid data.
+        """
         valid_files_path_list = [os.path.join(self.data_validation_config.valid_training_data_dir,file) for file in os.listdir(self.data_validation_config.valid_training_data_dir)]
         invalid_files_path_list = [os.path.join(self.data_validation_config.invalid_training_data_dir,file) for file in os.listdir(self.data_validation_config.invalid_training_data_dir)]
         
@@ -187,6 +279,13 @@ class DataValidationComponent:
 
     @handle_exceptions
     def post_validation_deletion(self):
+        """
+        Deletes local valid and invalid data directories.
+
+        Description:
+            This method deletes the local directories containing valid and invalid data
+            after they have been successfully processed and ingested into MongoDB.
+        """
         self.log_writer.handle_logging("DELETING VALID & INVALID FILES FROM LOCAL!")
         shutil.rmtree(self.data_validation_config.valid_training_data_dir)
         shutil.rmtree(self.data_validation_config.invalid_training_data_dir)
@@ -195,6 +294,13 @@ class DataValidationComponent:
 
     @handle_exceptions
     def post_validation_ingestion(self):
+        """
+        Ingests valid data files from MongoDB into the local directory.
+
+        Description:
+            This method retrieves valid data files from MongoDB and stores them in the local
+            directory specified for valid data.
+        """
         self.log_writer.handle_logging("INGESTING VALID FILES FROM MONGODB")
         target_dir = self.data_validation_config.valid_training_data_dir
         self.mongo_connect.ingest_with_fs(target_dir,self.data_validation_config.mongo_valid_collection_name)
@@ -202,7 +308,20 @@ class DataValidationComponent:
 
 
     @handle_exceptions
-    def run_data_validation(self):
+    def run_data_validation(self)-> DataValidationArtifact:
+        """
+        Run data validation checks for training data.
+
+        Description:
+            This method performs data validation checks on the training data files found in
+            the configured feature store directory. It includes checks for file names, columns,
+            missing values, and zero standard deviations. Validated files are inserted into MongoDB
+            and removed from the local directory. An artifact containing directory paths for valid
+            and invalid data is returned.
+
+        Returns:
+            DataValidationArtifact: An artifact containing directory paths for valid and invalid data.
+        """
         
         #feature_store_dir = "C:/Users/ayhan\Desktop/predictive-wafer-maintenance/artifact/30_08_2023_14_02/data_ingestion/feature_store"
         self.log_writer.handle_logging("-------------ENTERED DATA VALIDATION STAGE------------")
@@ -210,10 +329,15 @@ class DataValidationComponent:
         feature_store_dir = self.data_ingestion_artifact.feature_store
         csv_path_list = os.listdir(feature_store_dir)
 
-        for csv_path in csv_path_list:
-            file_path = os.path.join(feature_store_dir,csv_path)
+        for csv_name in csv_path_list:
+            print(csv_name)
+            file_path = os.path.join(feature_store_dir,csv_name)
             self.check_all_conditions(file_path,is_prediction=False)
+
+        self.log_writer.handle_logging("CONDITIONAL CHECKS COMPLETED!")
         
+        
+
         self.post_validation_insertion()
         self.post_validation_deletion()
         self.post_validation_ingestion()
@@ -224,11 +348,25 @@ class DataValidationComponent:
             valid_unseen_data_dir= self.data_validation_config.valid_unseen_data_dir,
             invalid_unseen_data_dir= self.data_validation_config.invalid_unseen_data_dir
         )   
+        self.log_writer.handle_logging("Data Validation Artifact Created Succesfully..")
         return data_validation_artifact
     
 
     @handle_exceptions
-    def run_prediction_data_validation(self):
+    def run_prediction_data_validation(self)-> DataValidationArtifact:
+        """
+        Run data validation checks for prediction data.
+
+        Description:
+            This method performs data validation checks on the prediction data files found in
+            the configured prediction data directory. It includes checks for file names, columns,
+            missing values, and zero standard deviations. Validated files are inserted into MongoDB.
+            An artifact containing directory paths for valid and invalid data is returned.
+
+        Returns:
+            DataValidationArtifact: An artifact containing directory paths for valid and invalid data.
+        """
+
         self.log_writer.handle_logging("-------------ENTERED DATA VALIDATION STAGE FOR PREDICTION DATASET------------")
         prediction_files_dir = self.data_validation_config.prediction_data_dir
         csv_path_list = os.listdir(prediction_files_dir)
@@ -237,12 +375,15 @@ class DataValidationComponent:
             file_path = os.path.join(prediction_files_dir,csv_name)
             self.check_all_conditions(file_path,is_prediction=True)
 
+        self.log_writer.handle_logging("CONDITIONAL CHECKS COMPLETED!")
+
         data_validation_artifact = DataValidationArtifact(
             valid_data_dir= self.data_validation_config.valid_training_data_dir ,
             invalid_data_dir= self.data_validation_config.invalid_training_data_dir, 
             valid_unseen_data_dir= self.data_validation_config.valid_unseen_data_dir,
             invalid_unseen_data_dir= self.data_validation_config.invalid_unseen_data_dir
         )  
+        self.log_writer.handle_logging("Data Validation Artifact Created Succesfully..")
 
         return data_validation_artifact
 
